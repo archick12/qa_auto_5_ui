7a87122d9d2c0eefc461ec7a634bbedce46f296d
marinamv <marina093746075@gmail.com>
2018-01-18 11:04:51 +0200

    Added Locators for Description Added comments for Locators Changed the order of Locators

--- src/main/java/ui/pages/NewIssuePage.java
+++ src/main/java/ui/pages/NewIssuePage.java
@@ -6,9 +6,18 @@ import ui.utils.RemoteDriverManager;
 public class NewIssuePage extends BasePage {
 
     private HeaderPage headerPage;
-
+    //Old Locators
+    //Creating Sub-Task inside ticket
+    //private By newSubtaskButtonLocator = By.id("stqc_show");
+    //private By subtaskLocator = By.linkText("Snizhanna test"); - should be changed for a specific test
+    //private By moreButtonLocator = By.id("opsbar-operations_more");
+    //private By deleteListItemLocator = By.id("delete-issue"); - not found
+    //private By deleteButtonLocator = By.id("delete-issue-submit"); - not found
+    //private By successPopUp = By.xpath("//*[contains(@class,'aui-message-success')]");
+    //Create a New Issue Button
+    private By createLocator = By.id("create_link");
+    //Configure Fields
     private By configureFiedlsLocator = By.id("qf-field-picker-trigger");
-// Locators for all check-boxes in PopUp Configure Fields
     private By showFieldsAllLocator = By.xpath("//*[contains(@class,'qf-unconfigurable')]//a[.='All']");
     private By showFieldsCustomLocator = By.xpath("//*[contains(@class,'qf-unconfigurable')]//a[.='Custom']");
     private By selectAssigneeLocator = By.xpath("//*[contains(@class,'qf-picker-button')]//a[.='Assignee']");
@@ -19,48 +28,42 @@ public class NewIssuePage extends BasePage {
     private By selectLabelsLocator = By.xpath("//*[contains(@class,'qf-picker-button')]//a[.='Labels']");
     private By selectPriorityLocator = By.xpath("//*[contains(@class,'qf-picker-button')]//a[.='Priority']");
     private By selectLinkToEpicLocator = By.xpath("//*[contains(@class,'qf-picker-button')]//a[.='Ссылка на эпик']");
-
-    //Locators for create Issue
+    //Project
     private By fieldProjectLocator = By.xpath("//*[@id='project-field']");
+    //Issue Type
     private By issueTypeLocator = By.xpath("//*[@id='issuetype-field']");
+    //Epic Name
     private By nameEpicLocator = By.xpath("//*[@id='customfield_10002']");
-    private By summaryFieldLocator = By.xpath("//*[@id='summary']");
-
-    private By assigneeFieldLocator = By.xpath("//*[@id='assignee-field']");
-    private By assignToMeButtonLocator = By.xpath("(//*[@id='assign-to-me-trigger']");
-    private By createButtonLocator = By.xpath("//*[@id='create-issue-submit']");
-    private By createAnotherButtonLocator = By.xpath("//*[@id='qf-create-another']");
-    private By linktoEpicFieldLocator = By.xpath("//*[@id='customfield_10000-field']");
-    private By cancelButtonLocator = By.xpath("//*[@title='Press undefined+` to cancel']");
-    private By dropAssigneeFieldLocator = By.xpath("//*[@id='assignee-field']//a[text()='unassigned-2']");
-    private By dropCustomFieldArtefacts = By.xpath("//*[@id='customfield_10000']//a[text()='artefacts']");
-    private By getDropCustomFieldBonusshop = By.xpath("//*[@id='customfield_10000']//a[text()='bonusshop']");
-    private By getDropCustomFieldExtraapps = By.xpath("//*[@id='customfield_10000']//a[text()='extraaps']");
-
-
-
-
-
-
-
-
-    private By newSubtaskButtonLocator = By.id("stqc_show");
+    //Summary
     private By summaryLocator = By.id("summary");
-    private By submitButtonLocator = By.id("create-issue-submit");
-    private By subtaskLocator = By.linkText("Snizhanna test");
-    private By moreButtonLocator = By.id("opsbar-operations_more");
-    private By deleteListItemLocator = By.id("delete-issue");
-    private By deleteButtonLocator = By.id("delete-issue-submit");
-    private By successPopUp = By.xpath("//*[contains(@class,'aui-message-success')]");
-
-
-    // Locators for CreateIssue
+    //Description
+    private By menuStyle = By.xpath("//*[@field-id='description']//*[text()='Style']");
+    private By iconBold = By.xpath("//*[@field-id='description']//*[@data-operation='bold']");
+    private By iconItalic =By.xpath("//*[@field-id='description']//*[@data-operation='italic']");
+    private By iconUnderline = By.xpath("//*[@field-id='description']//*[@data-operation='underline']");
+    private By iconTextColor = By.xpath("//*[@field-id='description']//*[@original-title='Text color']");
+    private By moreColors = By.xpath("//*[@field-id='description']//*[@data-operation='color-parameter']");
+    private By menuMore = By.xpath( "//*[@field-id='description']//*[@original-title='More']");
+    private By menuLink = By.xpath("//*[@field-id='description']//*[@original-title='Link (Ctrl+K)']");
+    private By menuAttachment = By.xpath("//*[@field-id='description']//*[@original-title='Attachment']");
+    private By iconBulletList= By.xpath("//*[@field-id='description']//*[@original-title='Bullet list (Ctrl+Shift+B)']");
+    private By iconNumberedList = By.xpath("//*[@field-id='description']//*[@original-title='Numbered list (Ctrl+Shift+N)']");
+    private By menuIcons = By.xpath("//*[@field-id='description']//*[@original-title='Icons']");
+    private By menuOther = By.xpath("//*[@field-id='description']//*[@original-title='Other']");
+    private By iconCollapseExpand = By.xpath("//*[@field-id='description']//*[@original-title='Toggle visibility']");
+    private By editDescription = By.id("description-wiki-edit");
+    private By tabVisual= By.xpath("//*[@data-mode='wysiwyg']");
+    private By tabText= By.xpath("//*[@data-mode='source']");
+    private By iconUndo = By.xpath("//*[@title='Undo']");
+    private By iconRedo = By.xpath("//*[@title='Redo']");
+    // Priority
     private By localHelp = By.xpath("//*[@id='priority-single-select']//following::span[contains(@class,'aui-iconfont-help')]");
     private By priorityFieldDefault = By.xpath("//*[@id='priority-field']");
     private By prioritySelectHighest = By.xpath("//*[@id='priority-single-select']//a[text()='Highest']");
     private By prioritySelectHigh = By.xpath("//*[@id='priority-single-select']//a[text()='High']");
     private By prioritySelectLow = By.xpath("//*[@id='priority-single-select']//a[text()='Low']");
     private By prioritySelectLowest = By.xpath("//*[@id='priority-single-select']//a[text()='Lowest']");
+    //Linked Issue
     private By linkedIssuesDefault = By.xpath("//*[@id='issuelinks-linktype']");
     private By linkedIssuesIsBlockedBy = By.xpath("//*[@id='issuelinks-linktype'] //*[@value='is blocked by']");
     private By linkedIssuesIsClones = By.xpath("//*[@id='issuelinks-linktype'] //*[@value='clones']");
@@ -68,13 +71,14 @@ public class NewIssuePage extends BasePage {
     private By linkedIssuesDuplicates = By.xpath("//*[@id='issuelinks-linktype'] //*[@value='duplicates']");
     private By linkedIssuesIsDuplicatedBy = By.xpath("//*[@id='issuelinks-linktype'] //*[@value='is duplicated by']");
     private By linkedIssuesRelatesTo = By.xpath("//*[@id='issuelinks-linktype'] //*[@value='relates to']");
+    //Labels
     private By labelsSelect = By.xpath("//*[@id='labels-multi-select']");
+    //Issue
     private By IssuelinksSelect = By.xpath("//*[@id='issuelinks-issues-multi-select']");
     private By IssuePopup = By.xpath("//a[@class='issue-picker-popup']");
+    //Attachment
     private By browseButton = By.xpath("//*[@class='form-body']//*[@class='issue-drop-zone__button']");
-
-
-   //Locators for IssueSelector
+    //Locators for IssueSelector Pop-up
     private By pageHeader = By.xpath("//*[@class='aui-page-header-main']");
     private By selectButton = By.xpath("//*[@class='aui-button']");
     private By sourceRecent = By.xpath("//*[@id='issue-source-recent']");
@@ -82,6 +86,19 @@ public class NewIssuePage extends BasePage {
     private By searchField = By.xpath("//*[@id='searchRequestId']");
     private By viewedIssues = By.xpath("//*[text()='Issues you have recently viewed']");
     private By first50Issues = By.xpath("//*[text()='First 50 issues from your current search']");
+    //Assignee
+    private By assigneeFieldLocator = By.xpath("//*[@id='assignee-field']");
+    //Assign to me
+    private By assignToMeButtonLocator = By.xpath("(//*[@id='assign-to-me-trigger']");
+    //Create
+    private By submitButtonLocator = By.id("create-issue-submit");
+    //Create another
+    private By createAnotherButtonLocator = By.xpath("//*[@id='qf-create-another']");
+    //Epic Link
+    private By linktoEpicFieldLocator = By.xpath("//*[@id='customfield_10000-field']");
+    //Cancel
+    private By cancelButtonLocator = By.xpath("//*[@title='Press undefined+` to cancel']");
+
 
 
     public NewIssuePage() {
