79becbd16f02309e4b3316ddf976efa931da9a70
try-to-pick-unique-username <alesyakovalevich@gmail.com>
2018-02-01 15:12:48 +0200

    merged Eugene test into team tests

--- src/test/java/ui/CreateIssueTest.java
+++ src/test/java/ui/CreateIssueTest.java
@@ -39,6 +39,7 @@ public class CreateIssueTest {
   }
 
 
+  // Alesya's test
   @TestCase(id = "C13") // annotation that holds number of test case in TestRail
   @Test(groups = {"UI","CreateIssue","SKIP"}) // annotation that helps to mark method as test and include pr exclude them by group name in testng.xml
   public void createNewStory() throws InterruptedException {
@@ -96,6 +97,36 @@ public class CreateIssueTest {
             .selectPriority(issuePriority)
             .clickSubmitButton()
             .clickNewIssueLinkOnSuccessPopup();
+
+  }
+
+  // Eugene's test
+  @TestCase(id = "C10") // annotation that holds number of test case in TestRail
+  @Test(groups = {"UI","CreateIssue"}) // annotation that helps to mark method as test and include pr exclude them by group name in testng.xml
+  public void createNewEpic() throws InterruptedException {
+
+    NewIssuePage newIssuePage = new NewIssuePage();
+    IssuePage issuePage = new IssuePage();
+
+    String projectId = "QAAuto5";
+    String issueType = "Epic";
+    String epicName = "Team 2 Epic имя эпика";
+    String epicSummary = "Team 2 Epic summary";
+    String epicDescription = "Team 2 Epic description";
+    String issuePriority = "Medium";
+    String issueLabel = "QAAuto5";
+
+    // create new issue
+    newIssuePage
+            .enterProject(projectId)
+            .enterIssueType(issueType)
+            .fillNameEpic(epicName)
+            .fillSummary(epicSummary)
+            .fillDescription(epicDescription)
+            .selectPriority(issuePriority)
+            .addLabel(issueLabel)
+            .assignUser()
+            .clickSubmitButton();
 
   }
 
