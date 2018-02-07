51c051c568c0a95e90a1586dd7da493974793a4b
apiliuk <lohika.artur@98point6.com>
2018-02-07 16:47:11 +0200

    added sample for changes in @Test layer for Smoke Test

---
+++ src/test/java/ui/TestNGGroups.java
@@ -0,0 +1,32 @@
+package ui;
+
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+public class TestNGGroups
+{
+  @BeforeGroups("Smoke Test, Regression")
+  public void setup()
+  {
+    System.out.println("Launching URL");
+  }
+
+  @Test(groups={"Smoke Test"})
+  public void method1()
+  {
+    System.out.println("Smoke");
+  }
+
+  @Test(groups={"Regression"}, dependsOnGroups = {"Smoke Test"})
+  public void method2()
+  {
+    System.out.println("Regression-1");
+  }
+
+  @Test(groups={"Regression"}, dependsOnGroups = {"Smoke Test"})
+  public void method3()
+  {
+    System.out.println("Regression-2");
+  }
+}
